9141ac59662c84872ba6e47f1ce5a968dd312871
Grayson Smith <d.grayson.smith@gmail.com>
2021-05-15 20:02:31 -0700

    Add watchos target

---
+++ ShareClient-watchos/ShareClient_watchos.h
@@ -0,0 +1,19 @@
+//
+//  ShareClient_watchos.h
+//  ShareClient-watchos
+//
+//  Created by Grayson Smith on 5/15/21.
+//  Copyright © 2021 Mark Wilson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShareClient_watchos.
+FOUNDATION_EXPORT double ShareClient_watchosVersionNumber;
+
+//! Project version string for ShareClient_watchos.
+FOUNDATION_EXPORT const unsigned char ShareClient_watchosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShareClient_watchos/PublicHeader.h>
+
+
